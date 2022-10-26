6da74f60fd64c05e297b356a985ebb477d1db3f7
Sajid Sarker <sajid.sarker@gmail.com>
2022-10-26 03:40:45 +0330

    ETL pipeline, ML pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,62 @@
 import sys
 
+import numpy as np
+import pandas as pd
+
+from sqlalchemy import create_engine
+
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    Args:
+        messages_filepath (str): The file path for csv file containing messages
+        categories_filepath (str): The file path for csv file containing categories
+    Return:
+        df (DataFrame): A merged dataframe of messages and categories
+    '''
+    messages = pd.read_csv(messaged_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on='id')
+
+    return df
 
 
 def clean_data(df):
-    pass
+    '''
+    Args:
+        df (DataFrame): A merged dataframe of messages and categories
+    Return:
+        df (DataFrame): A merged and cleaned dataframe of messages and categories
+    '''
+    # Convert categories into categorical variables
+    categories = df['categories'].copy().str.split(pat=';', expand=True)
+    categories.columns = [x[0:-2] for x in categories.iloc[0, :]]
+    for column in categories:
+        categories[column] = categories[column].str[-1]
+        categories[column] = categories[column].astype(int)
+    categories['id'] = df['id'].copy()
+    df.drop('categories', inplace=True, axis=1)
+    df = df.merge(categories, on='id')
+
+    # Remove duplicates
+    duplicates = df.duplicated(subset=['id', 'message'], keep='first')
+    df = df[duplicates == False]
+    
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    '''
+    Args:
+        df (DataFrame): A merged and cleaned dataframe of messages and categories
+        database_filename (str): The file name for the database
+    Return:
+        None
+    '''
+    engine = create_engine('sqlite:///{}.db'.format(database_filename))
+    df.to_sql('Messages', engine, index=False)
+    
+    return None
 
 
 def main():
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,154 @@
 import sys
+import re
+import pickle
+
+import numpy as np
+import pandas as pd
+
+from sqlalchemy import create_engine
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+from sklearn.pipeline import Pipeline
+from sklearn.pipeline import FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.multioutput import MultiOutputClassifier
 
 
 def load_data(database_filepath):
-    pass
+    '''
+    Args:
+        ... (): ...
+    Return:
+        ... (): ...
+    '''
+    engine = create_engine(database_filepath)
+
+    df = pd.read_sql_table('Messages', engine)
+
+    X = df['message'].copy()
+
+    Y = df.columns
+    Y = Y.drop(['id', 'message', 'original', 'genre'])
+    Y = df[Y].copy()
+
+    return X, Y, Y.columns
 
 
 def tokenize(text):
-    pass
+    '''
+    Args:
+        text (str): ...
+    Return:
+        ... (): ...
+    '''
+    # Normalise text
+    text = text.lower()
+
+    # Remove URLs
+    url_mask = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+    urls = re.findall(url_mask, text)
+    for url in urls:
+        text = text.replace(url, 'urlplaceholder')
+
+    # Remove punctuation
+    text = re.sub(r'[^a-zA-Z0-9]', ' ', text)
+
+    # Tokenise text
+    tokens = word_tokenize(text)
+
+    # Remove stopwords
+    tokens = [token for token in tokens if token not in stopwords.words('english')]
+
+    # Stemming
+    stemmer = PorterStemmer()
+    stemmed_tokens = [stemmer.stem(token) for token in tokens]
+
+    # Lemmatisation
+    lemmatiser = WordNetLemmatizer()
+    lemmed_tokens = [lemmatiser.lemmatize(token) for token in stemmed_tokens]
+    lemmed_tokens = [lemmatiser.lemmatize(token, pos='v') for token in lemmed_tokens]
+
+    clean_tokens = lemmed_tokens
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    '''
+    Args:
+        ... (): ...
+    Return:
+        ... (): ...
+    '''
+    # Create model pipeline
+    pipeline = Pipeline([
+
+        ('nlp_pipeline', Pipeline([
+            ('vectorise', CountVectorizer(tokenizer=tokenize)),
+            ('tfidf', TfidfTransformer())
+        ])),
+
+        ('classifier', MultiOutputClassifier(RandomForestClassifier()))
+
+    ])
+
+    # Define grid search parameters
+    parameters = {
+        '': ,
+        '': (
+            {'': },
+            {'': },
+            {'': }
+        )
+    }
+
+    cross_validated_model = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cross_validated_model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''
+    Args:
+        ... (): ...
+    Return:
+        ... (): ...
+    '''
+    Y_pred = model.predict(X_test)
+    accuracy = (Y_pred == Y_test.values).mean()
+    print('Model Accuracy: {:.3f}%'.format(accuracy * 100))
+
+    return None
 
 
 def save_model(model, model_filepath):
-    pass
+    '''
+    Args:
+        model (): The classification model
+        model_filepath (str): The file path for classification model pickle file
+    Return:
+        None
+    '''
+    with open(model_filepath, 'wb') as model_file:
+        pickle.dump(model, model_file)
+        model_file.close()
+
+    return None
 
 
 def main():
